57d394572b0ffd9f77c95cdf11b5a73b1580083c
Unchosen <unchosen.aus@gmail.com>
2017-11-02 17:40:26 +0800

    Minor Mixin Improvements

--- index.js
+++ index.js
@@ -22,12 +22,11 @@ export const mixinPropertiesAttributes = (base,propertiesName='properties') => c
 	
 	constructor({ protectedProperties=[], propertyStore={}, onPropertySet, superArguments=[] }={}) {
 		super(...superArguments);
-		let element = this;
-		let propsConfig = element.constructor[propertiesName] || {};
+		let element = this, propsConfig = element.constructor[propertiesName] || {};
 		let protoTree = [], checkObj = null;
 		while(true){
 			checkObj = Object.getPrototypeOf(checkObj===null?element:checkObj);
-			if(!checkObj || !checkObj.constructor || checkObj.constructor===HTMLElement || checkObj.constructor===Function || checkObj.constructor===checkObj.constructor.constructor) break;
+			if(!checkObj || !checkObj.constructor || checkObj.constructor===HTMLElement || checkObj.constructor===Function || checkObj.constructor===Object || checkObj.constructor===checkObj.constructor.constructor) break;
 			protoTree.push(checkObj);
 		}
 		Object.freeze(propsConfig);
@@ -51,11 +50,10 @@ export const mixinPropertiesAttributes = (base,propertiesName='properties') => c
 				});
 			}
 			else {
-				let hasObserver = 'observer' in config;
-				let isObserverString = hasObserver && typeof config.observer==='string';
-				let safeAttributeType = config.type===String || config.type===Number || config.type===Boolean;
-				let reflectToAttribute = 'reflectToAttribute' in config ? config.reflectToAttribute : safeAttributeType;
-				let reflectFromAttribute = 'reflectFromAttribute' in config ? config.reflectFromAttribute : safeAttributeType;
+				let hasObserver = 'observer' in config, isObserverString = hasObserver && typeof config.observer==='string';
+				let isString = config.type===String, isNumber = config.type===Number, isBoolean = config.type===Boolean;
+				let reflectToAttribute = 'reflectToAttribute' in config ? config.reflectToAttribute : (isString || isNumber || isBoolean);
+				let reflectFromAttribute = 'reflectFromAttribute' in config ? config.reflectFromAttribute : (isString || isNumber || isBoolean);
 				Object.defineProperty(element,name,{
 					enumerable: true,
 					configurable: true,
@@ -63,32 +61,34 @@ export const mixinPropertiesAttributes = (base,propertiesName='properties') => c
 						if(propertyStore.hasOwnProperty(name)) return propertyStore[name];
 						if(reflectFromAttribute){
 							let hasAttribute = element.hasAttribute(name);
-							if(config.type===Boolean) return propertyStore[name] = hasAttribute;
-							if(hasAttribute && config.type===Number) return propertyStore[name] = Number(element.getAttribute(name));
+							if(isBoolean) return propertyStore[name] = hasAttribute;
+							if(hasAttribute && isNumber) return propertyStore[name] = Number(element.getAttribute(name));
 							if(hasAttribute) return propertyStore[name] = element.getAttribute(name);
 						}
 						return config.value;
 					},
 					set(newValue){
-						if(config.type===Boolean) newValue = !!newValue;
-						else if(config.type===Number) newValue = newValue===null || newValue===void 0 ? 0 : Number(newValue);
-						else if(config.type===String) newValue = newValue===null || newValue===void 0 ? '' : ''+newValue;
+						if(isBoolean) newValue = !!newValue;
+						else if(isNumber) newValue = newValue===null || newValue===void 0 ? 0 : Number(newValue);
+						else if(isString) newValue = newValue===null || newValue===void 0 ? '' : ''+newValue;
 						let oldValue = element[name];
 						if(oldValue===newValue) return;
-						if(hasObserver || onPropertySet || config.notify) var detailObj = { element,name,config,newValue,oldValue };
 						propertyStore[name] = newValue;
 						if(reflectToAttribute){
-							if(config.type===Boolean){
+							if(isBoolean){
 								let hasAttribute = element.hasAttribute(name);
 								if(!newValue && hasAttribute) element.removeAttribute(name);
 								if(newValue && !hasAttribute) element.setAttribute(name,'');
 							}
 							else element.setAttribute(name,newValue);
 						}
-						if(onPropertySet) onPropertySet(detailObj);
-						if(hasObserver && isObserverString) element[config.observer](detailObj);
-						else if(hasObserver) config.observer(detailObj);
-						if(config.notify) element.dispatchEvent(new CustomEvent(name+'-changed',{ detail:detailObj }));
+						if(onPropertySet || hasObserver || config.notify){
+							let detailObj = { element,name,config,newValue,oldValue };
+							if(onPropertySet) onPropertySet(detailObj);
+							if(hasObserver && isObserverString) element[config.observer](detailObj);
+							else if(hasObserver) config.observer(detailObj);
+							if(config.notify) element.dispatchEvent(new CustomEvent(name+'-changed',{ detail:detailObj }));
+						}
 					}
 				});
 			}
