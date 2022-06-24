af6cb834efd4040df3139856e94e3be1924d5187
Jason <unchosen.aus@gmail.com>
2022-06-24 18:41:34 +0800

    Cypress v10

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    "e2e": {
+        "baseUrl": "http://localhost:8181/tests/",
+        "supportFile": false,
+        "specPattern": "tests/**/*.test.js",
+    },
+    "includeShadowDom": true,
+    "video": false,
+});
