1dbd4dd5c9f02ffd568fd9105e945d79be76c0e9
Jason <unchosen.aus@gmail.com>
2022-11-10 03:49:15 +0800

    Improve Tests

--- tests/1-basic.test.js
+++ tests/1-basic.test.js
@@ -1,100 +1,112 @@
 "use strict";
 
+let ignoreUncaughtErrors = false, lastUncaughtError = null;
+Cypress.on('uncaught:exception', (err, runnable) => {
+	// returning false here prevents Cypress from failing the test
+	lastUncaughtError = err;
+	return !ignoreUncaughtErrors;
+});
+
 describe("Basic Features",()=>{
-	//beforeEach(()=>{ cy.visit('/1.html'); });
+	beforeEach(()=>{
+		ignoreUncaughtErrors = false;
+		lastUncaughtError = null;
+	});
 
 	it("Blank Element",()=>{
 		cy.visit('/simple/blank.html');
-		cy.get('#testElement').should('be.visible');
-		cy.get('#testElement').should('have.text', 'Element Content');
+		cy.get('#testElement')
+			.should('be.visible')
+			.then(([e])=>{
+				expect(e.innerText, 'Content').to.eq('Element Content');
+			});
 	});
 
 	it("Simple Element",()=>{
 		cy.visit('/simple/index.html');
-		cy.get('#testElement').should('be.visible');
-		cy.get('#testElement').should('have.text', 'Hello, Test');
+		cy.get('#testElement')
+			.should('be.visible')
+			.then(([e])=>{
+				expect(e.innerText, 'Content').to.eq('Hello, Test');
+			});
 	});
 
 	it("Simple Property & Attribute - Attribute defined in html",()=>{
 		cy.visit('/simple/index.html');
-		cy.get('#testElement').should('have.attr', 'name', 'Test');
-		cy.get('#testElement').should('have.text', 'Hello, Test');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.name, 'name').to.exist;
-			expect(e.name, 'name').to.eq('Test');
-		});
+		cy.get('#testElement')
+			.should('have.attr', 'name', 'Test')
+			.then(([e])=>{
+				expect(e.innerText, 'Content').to.eq('Hello, Test');
+				expect(e.name, 'name').to.exist;
+				expect(e.name, 'name').to.eq('Test');
+			});
 	});
 
 	it("Default Property & Attribute",()=>{
 		cy.visit('/simple/noAttrib.html');
-		cy.get('#testElement').should('have.attr', 'name', 'Default');
-		cy.get('#testElement').should('have.text', 'Hello, Default');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.name, 'name').to.exist;
-			expect(e.name, 'name').to.eq('Default');
-		});
+		cy.get('#testElement')
+			.should('have.attr', 'name', 'Default')
+			.then(([e])=>{
+				expect(e.innerText, 'Content').to.eq('Hello, Default');
+				expect(e.name, 'name').to.exist;
+				expect(e.name, 'name').to.eq('Default');
+			});
 	});
 
 	it("Change Property",()=>{
 		cy.visit('/simple/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.name, 'name').to.eq('Test');
-			e.name = 'Changed';
-		});
-		cy.get('#testElement').should('have.attr', 'name', 'Changed');
-		cy.get('#testElement').should('have.text', 'Hello, Changed');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.name, 'name').to.eq('Changed');
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.name, 'name').to.eq('Test');
+				e.name = 'Changed';
+			})
+			.should('have.attr', 'name', 'Changed')
+			.then(([e])=>{
+				expect(e.innerText, 'Changed Content').to.eq('Hello, Changed');
+				expect(e.name, 'name').to.eq('Changed');
+			});
 	});
 
 	it("Change Attribute",()=>{
 		cy.visit('/simple/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.name, 'name').to.eq('Test');
-			e.setAttribute('name', 'Changed');
-		});
-		cy.get('#testElement').should('have.attr', 'name', 'Changed');
-		cy.get('#testElement').should('have.text', 'Hello, Changed');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.name, 'name').to.eq('Changed');
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.name, 'name').to.eq('Test');
+				e.setAttribute('name', 'Changed');
+			})
+			.should('have.attr', 'name', 'Changed')
+			.then(([e])=>{
+				expect(e.innerText, 'Changed Content').to.eq('Hello, Changed');
+				expect(e.name, 'name').to.eq('Changed');
+			});
 	});
 
 	it("Property & Attribute with mixed case",()=>{
 		cy.visit('/simple/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.mixedCaseProp = 'Changed';
-			expect(e.mixedCaseProp, 'Change Value via Property').to.eq('Changed');
-			e.setAttribute('mixedCaseProp', 'Changed2');
-			expect(e.mixedCaseProp, 'Change Value via Attribute as mixed case').to.eq('Changed2');
-		});
-		cy.get('#testElement').should('have.attr', 'mixedCaseProp', 'Changed2');
-		cy.get('#testElement').should('have.attr', 'mixedcaseprop', 'Changed2');
-		cy.get('#testElement').should('have.attr', 'MIXEDcasePROP', 'Changed2');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.setAttribute('mixedcaseprop', 'Changed3');
-			expect(e.mixedCaseProp, 'Change Value via Attribute as lowercase').to.eq('Changed3');
-		});
-		cy.get('#testElement').should('have.attr', 'mixedCaseProp', 'Changed3');
-		cy.get('#testElement').should('have.attr', 'mixedcaseprop', 'Changed3');
-		cy.get('#testElement').should('have.attr', 'MIXEDcasePROP', 'Changed3');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.setAttribute('MIXEDcasePROP', 'Changed4');
-			expect(e.mixedCaseProp, 'Change Value via Attribute as mixed case').to.eq('Changed4');
-		});
-		cy.get('#testElement').should('have.attr', 'mixedCaseProp', 'Changed4');
-		cy.get('#testElement').should('have.attr', 'mixedcaseprop', 'Changed4');
-		cy.get('#testElement').should('have.attr', 'MIXEDcasePROP', 'Changed4');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.mixedCaseProp = 'Changed';
+				expect(e.mixedCaseProp, 'Change Value via Property').to.eq('Changed');
+				e.setAttribute('mixedCaseProp', 'Changed2');
+				expect(e.mixedCaseProp, 'Change Value via Attribute as mixed case').to.eq('Changed2');
+			})
+			.should('have.attr', 'mixedCaseProp', 'Changed2')
+			.should('have.attr', 'mixedcaseprop', 'Changed2')
+			.should('have.attr', 'MIXEDcasePROP', 'Changed2')
+			.then(([e])=>{
+				e.setAttribute('mixedcaseprop', 'Changed3');
+				expect(e.mixedCaseProp, 'Change Value via Attribute as lowercase').to.eq('Changed3');
+			})
+			.should('have.attr', 'mixedCaseProp', 'Changed3')
+			.should('have.attr', 'mixedcaseprop', 'Changed3')
+			.should('have.attr', 'MIXEDcasePROP', 'Changed3')
+			.then(([e])=>{
+				e.setAttribute('MIXEDcasePROP', 'Changed4');
+				expect(e.mixedCaseProp, 'Change Value via Attribute as mixed case').to.eq('Changed4');
+			})
+			.should('have.attr', 'mixedCaseProp', 'Changed4')
+			.should('have.attr', 'mixedcaseprop', 'Changed4')
+			.should('have.attr', 'MIXEDcasePROP', 'Changed4');
 	});
 
 });
--- tests/2-types.test.js
+++ tests/2-types.test.js
@@ -1,358 +1,381 @@
 "use strict";
 
+let ignoreUncaughtErrors = false, lastUncaughtError = null;
+Cypress.on('uncaught:exception', (err, runnable) => {
+	// returning false here prevents Cypress from failing the test
+	lastUncaughtError = err;
+	return !ignoreUncaughtErrors;
+});
+
 describe("Data Type Defaults",()=>{
+	beforeEach(()=>{
+		ignoreUncaughtErrors = false;
+		lastUncaughtError = null;
+	});
+
 	it("String No Default Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.strEmpty, 'strEmpty').to.eq('');
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.strEmpty, 'strEmpty').to.eq('');
+			});
 	});
 	
 	it("String No Default Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').should('have.attr', 'strEmpty', '');
+		cy.get('#testElement')
+			.should('have.attr', 'strEmpty', '');
 	});
 	
 	it("Number No Default Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.numEmpty, 'numEmpty').to.eq(0);
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.numEmpty, 'numEmpty').to.eq(0);
+			});
 	});
 	
 	it("Number No Default Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').should('have.attr', 'numEmpty', '0');
+		cy.get('#testElement')
+			.should('have.attr', 'numEmpty', '0');
 	});
 	
 	it("Boolean No Default Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.boolEmpty, 'boolEmpty').to.eq(false);
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.boolEmpty, 'boolEmpty').to.eq(false);
+			});
 	});
 	
 	it("Boolean No Default Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').should('not.have.attr', 'boolEmpty');
+		cy.get('#testElement')
+			.should('not.have.attr', 'boolEmpty', '');
 	});
 	
 	it("String Default Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.str, 'str').to.eq('Default String Value');
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.str, 'str').to.eq('Default String Value');
+			});
 	});
 	
 	it("String Default Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').should('have.attr', 'str', 'Default String Value');
+		cy.get('#testElement')
+			.should('have.attr', 'str', 'Default String Value');
 	});
 	
 	it("Number Default Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.num, 'num').to.eq(42);
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.num, 'num').to.eq(42);
+			});
 	});
 	
 	it("Number Default Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').should('have.attr', 'num', '42');
+		cy.get('#testElement')
+			.should('have.attr', 'num', '42');
 	});
 	
 	it("Boolean Default Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.bool, 'bool').to.eq(true);
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.bool, 'bool').to.eq(true);
+			});
 	});
 	
 	it("Boolean Default Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').should('have.attr', 'bool', '');
+		cy.get('#testElement')
+			.should('have.attr', 'bool', '');
 	});
 	
 	it("Combined Default Content",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').should('have.text', 'Str Default String Value, Num 42, Bool: true');
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.innerText, 'Content').to.eq('Str Default String Value, Num 42, Bool: true');
+			})
 	});
 	
 	it("Non-Implemented Default Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			expect(e.custom, 'custom').to.eq('Custom');
-		});
+		cy.get('#testElement')
+			.then(([e])=>{
+				expect(e.custom, 'custom').to.eq('Custom');
+			});
 	});
 	
 	it("Non-Implemented Default Attribute (none)",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').should('not.have.attr', 'custom');
+		cy.get('#testElement')
+			.should('not.have.attr', 'custom', '');
 	});
 });
 
 describe("Data Type Assigns",()=>{
-	//beforeEach(()=>{ cy.visit('/1.html'); });
+	beforeEach(()=>{
+		ignoreUncaughtErrors = false;
+		lastUncaughtError = null;
+	});
 	
 	it("String Change - via Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.str = 'New Value';
-			expect(e.str, 'str').to.eq('New Value');
-		});
-		cy.get('#testElement').should('have.attr', 'str', 'New Value');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.str = 'New Value';
+				expect(e.str, 'str').to.eq('New Value');
+			})
+			.should('have.attr', 'str', 'New Value');
 	});
 	
 	it("String Change - via Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.setAttribute('str', 'New Value');
-			expect(e.str, 'str').to.eq('New Value');
-		});
-		cy.get('#testElement').should('have.attr', 'str', 'New Value');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.setAttribute('str', 'New Value');
+				expect(e.str, 'str').to.eq('New Value');
+			})
+			.should('have.attr', 'str', 'New Value');
 	});
 	
 	it("Number Change - via Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.num = 69;
-			expect(e.num, 'num').to.eq(69);
-		});
-		cy.get('#testElement').should('have.attr', 'num', '69');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.num = 69;
+				expect(e.num, 'num').to.eq(69);
+			})
+			.should('have.attr', 'num', '69');
 	});
 	
 	it("Number Change - via Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.setAttribute('num', '69');
-			expect(e.num, 'num').to.eq(69);
-		});
-		cy.get('#testElement').should('have.attr', 'num', '69');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.setAttribute('num', '69');
+				expect(e.num, 'num').to.eq(69);
+			})
+			.should('have.attr', 'num', '69');
 	});
 	
 	it("Boolean Change to false - via Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = false;
-			expect(e.bool, 'bool').to.eq(false);
-		});
-		cy.get('#testElement').should('not.have.attr', 'bool');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.bool = false;
+				expect(e.bool, 'bool').to.eq(false);
+			})
+			.should('not.have.attr', 'bool', '');
 	});
 	
 	it("Boolean Change to false - via Attribute (remove)",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.removeAttribute('bool');
-			expect(e.bool, 'bool').to.eq(false);
-		});
-		cy.get('#testElement').should('not.have.attr', 'bool');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.removeAttribute('bool');
+				expect(e.bool, 'bool').to.eq(false);
+			})
+			.should('not.have.attr', 'bool', '');
 	});
 	
 	it("Boolean Change to false then true - via Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = false;
-			expect(e.bool, 'bool').to.eq(false);
-		});
-		cy.get('#testElement').should('not.have.attr', 'bool');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = true;
-			expect(e.bool, 'bool').to.eq(true);
-		});
-		cy.get('#testElement').should('have.attr', 'bool', '');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.bool = false;
+				expect(e.bool, 'bool').to.eq(false);
+			})
+			.should('not.have.attr', 'bool', '');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.bool = true;
+				expect(e.bool, 'bool').to.eq(true);
+			})
+			.should('have.attr', 'bool', '');
 	});
 	
 	it("Boolean Change to false then true - via Attribute (remove)",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.removeAttribute('bool');
-			expect(e.bool, 'bool').to.eq(false);
-		});
-		cy.get('#testElement').should('not.have.attr', 'bool');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.setAttribute('bool', '');
-			expect(e.bool, 'bool').to.eq(true);
-		});
-		cy.get('#testElement').should('have.attr', 'bool', '');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.removeAttribute('bool');
+				expect(e.bool, 'bool').to.eq(false);
+			})
+			.should('not.have.attr', 'bool', '')
+			.then(([e])=>{
+				e.setAttribute('bool', '');
+				expect(e.bool, 'bool').to.eq(true);
+			})
+			.should('have.attr', 'bool', '');
 	});
 	
 	it("Combined Changed Content - via Property",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.str = 'New Value';
-			e.num = 69;
-			e.bool = false;
-		});
-		cy.get('#testElement').should('have.text', 'Str New Value, Num 69, Bool: false');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.str = 'New Value';
+				e.num = 69;
+				e.bool = false;
+			})
+			.then(([e])=>{
+				expect(e.innerText, 'Changed Content').to.eq('Str New Value, Num 69, Bool: false');
+			});
 	});
 	
 	it("Combined Changed Content - via Attribute",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.setAttribute('str', 'New Value');
-			e.setAttribute('num', '69');
-			e.removeAttribute('bool');
-		});
-		cy.get('#testElement').should('have.text', 'Str New Value, Num 69, Bool: false');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.setAttribute('str', 'New Value');
+				e.setAttribute('num', '69');
+				e.removeAttribute('bool');
+			})
+			.then(([e])=>{
+				expect(e.innerText, 'Changed Content').to.eq('Str New Value, Num 69, Bool: false');
+			});
 	});
 	
 	it("String Set As Object",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.str = {};
-			expect(e.str, 'str').to.eq('' + {});
-		});
-		cy.get('#testElement').should('have.attr', 'str', '' + {});
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.str = {};
+				expect(e.str, 'str').to.eq('' + {});
+			})
+			.should('have.attr', 'str', '' + {});
 	});
 	
 	it("String Set Property As Number",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.str = 42;
-			expect(e.str, 'str').to.eq('42');
-		});
-		cy.get('#testElement').should('have.attr', 'str', '42');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.str = 42;
+				expect(e.str, 'str').to.eq('42');
+			})
+			.should('have.attr', 'str', '42');
 	});
 	
 	it("String Set Property As Boolean",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.str = false;
-			expect(e.str, 'str').to.eq('false');
-		});
-		cy.get('#testElement').should('have.attr', 'str', 'false');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.str = true;
-			expect(e.str, 'str').to.eq('true');
-		});
-		cy.get('#testElement').should('have.attr', 'str', 'true');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.str = false;
+				expect(e.str, 'str').to.eq('false');
+			})
+			.should('have.attr', 'str', 'false')
+			.then(([e])=>{
+				e.str = true;
+				expect(e.str, 'str').to.eq('true');
+			})
+			.should('have.attr', 'str', 'true');
 	});
 	
 	it("Number Set Property As String",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.num = '69';
-			expect(e.num, 'num').to.eq(69);
-		});
-		cy.get('#testElement').should('have.attr', 'num', '69');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.num = '69';
+				expect(e.num, 'num').to.eq(69);
+			})
+			.should('have.attr', 'num', '69');
 	});
 	
 	it("Number Set Property As Object",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.num = e;
-			expect(e.num, 'num').to.satisfy(Cypress._.isNaN);
-		});
-		cy.get('#testElement').should('have.attr', 'num', 'NaN');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.num = e;
+				expect(e.num, 'num').to.satisfy(Cypress._.isNaN);
+			})
+			.should('have.attr', 'num', 'NaN');
 	});
 	
 	it("Number Set Property As Boolean",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.num = true;
-			expect(e.num, 'num').to.eq(1);
-		});
-		cy.get('#testElement').should('have.attr', 'num', '1');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.num = false;
-			expect(e.num, 'num').to.eq(0);
-		});
-		cy.get('#testElement').should('have.attr', 'num', '0');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.num = true;
+				expect(e.num, 'num').to.eq(1);
+			})
+			.should('have.attr', 'num', '1')
+			.then(([e])=>{
+				e.num = false;
+				expect(e.num, 'num').to.eq(0);
+			})
+			.should('have.attr', 'num', '0');
 	});
 	
 	it("Boolean Set Property As String - truey/falsy",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = '1';
-			expect(e.bool, 'bool').to.eq(true);
-		});
-		cy.get('#testElement').should('have.attr', 'bool', '');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = '';
-			expect(e.bool, 'bool').to.eq(false);
-		});
-		cy.get('#testElement').should('not.have.attr', 'bool');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.bool = '1';
+				expect(e.bool, 'bool').to.eq(true);
+			})
+			.should('have.attr', 'bool', '')
+			.then(([e])=>{
+				e.bool = '';
+				expect(e.bool, 'bool').to.eq(false);
+			})
+			.should('not.have.attr', 'bool', '');
 	});
 	
 	it("Boolean Set Property As Number - truey/falsy",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = 1;
-			expect(e.bool, 'bool').to.eq(true);
-		});
-		cy.get('#testElement').should('have.attr', 'bool', '');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = 0;
-			expect(e.bool, 'bool').to.eq(false);
-		});
-		cy.get('#testElement').should('not.have.attr', 'bool');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.bool = 1;
+				expect(e.bool, 'bool').to.eq(true);
+			})
+			.should('have.attr', 'bool', '')
+			.then(([e])=>{
+				e.bool = 0;
+				expect(e.bool, 'bool').to.eq(false);
+			})
+			.should('not.have.attr', 'bool', '');
 	});
 	
 	it("Boolean Set Property As Object - truey/falsy",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = {};
-			expect(e.bool, 'bool').to.eq(true);
-		});
-		cy.get('#testElement').should('have.attr', 'bool', '');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.bool = null;
-			expect(e.bool, 'bool').to.eq(false);
-		});
-		cy.get('#testElement').should('not.have.attr', 'bool');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.bool = {};
+				expect(e.bool, 'bool').to.eq(true);
+			})
+			.should('have.attr', 'bool', '')
+			.then(([e])=>{
+				e.bool = null;
+				expect(e.bool, 'bool').to.eq(false);
+			})
+			.should('not.have.attr', 'bool', '');
 	});
 	
 	it("Non-Implemented Set Property As String",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.custom = 'New Value';
-			expect(e.custom, 'custom').to.eq('New Value');
-		});
-		cy.get('#testElement').should('not.have.attr', 'custom');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.custom = 'New Value';
+				expect(e.custom, 'custom').to.eq('New Value');
+			})
+			.should('not.have.attr', 'custom', '');
 	});
 	
 	it("Non-Implemented Set Attribute As String (no-op)",()=>{
 		cy.visit('/types/index.html');
-		cy.get('#testElement').then($e=>{
-			let e = $e[0];
-			e.setAttribute('custom', 'New Value');
-			expect(e.custom, 'custom').to.eq('Custom');
-		});
-		cy.get('#testElement').should('have.attr', 'custom', 'New Value');
+		cy.get('#testElement')
+			.then(([e])=>{
+				e.setAttribute('custom', 'New Value');
+				expect(e.custom, 'custom').to.eq('Custom');
+			})
+			.should('have.attr', 'custom', 'New Value');
 	});
 	
 });
