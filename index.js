7ef34e0b046c7b5a0cb48d0f78323da922d0e0ea
Jason <unchosen.aus@gmail.com>
2023-02-18 18:09:49 +0800

    Optimised constructor & other code

--- index.js
+++ index.js
@@ -60,8 +60,7 @@ export class mixinClass {
 
 	static ce_observedAttributes(propertiesName){
 		let propsConfig = mixinClass.buildProtoPropsConfig(propertiesName,mixinClass.getProtoTree(this));
-		return Object.keys(propsConfig).filter(name=>{
-			let config = propsConfig[name];
+		return Object.entries(propsConfig).filter(([name,config])=>{
 			if(config.readOnly) return false;
 			if('reflectFromAttribute' in config) return !!config.reflectFromAttribute;
 			let type = config.type;
@@ -69,8 +68,7 @@ export class mixinClass {
 			else if(type===String || type===Number || type===Boolean) return true;
 			if('fromAttribute' in Object(type)) return !!type.fromAttribute;
 			return false;
-		}).map(name=>{
-			let config = propsConfig[name];
+		}).map(([name,config])=>{
 			return ('attribute' in config ? config.attribute+'' : name).toLowerCase();
 		});
 	}
@@ -92,146 +90,148 @@ export class mixinClass {
 	}
 	
 	static ce_mixinConstructor(argOptions,propertiesName){
-		let { protectedProperties=[], protectedAttributes=[], propertyStore={}, onPropertySet, propertyDefaults={} } = Object(argOptions);
-		let element = this;
+		argOptions = Object(argOptions);
 		let protoTree = mixinClass.getConstructorTree(Object.getPrototypeOf(this));
 		let propsConfig = mixinClass.buildConstructorPropsConfig(propertiesName,protoTree);
 		let propsLower = Object.keys(propsConfig).map(prop=>prop.toLowerCase());
 		for(let i=0,l=propsLower.length; i<l; i++){
 			if(propsLower.indexOf(propsLower[i])!==i) throw new Error(`Unable to setup property/attribute '${propsLower[i]}' on ${this.constructor.name}. It is a duplicate property (not case sensitive).`);
 		}
-		protectedAttributes = protectedAttributes.map(a=>a.toLowerCase());
-		let propsConfigByAttribute = this[mixinClass.symbols.propsConfigByAttribute] = {};
-		let hasConfigDefaults = Object.keys(propertyDefaults).length>0;
-		Object.keys(propsConfig)
-		.sort((a,b)=>{
-			let ac = propsConfig[a], bc = propsConfig[b], ao = 'order' in ac, bo = 'order' in bc;
+		argOptions.protectedAttributes = (argOptions.protectedAttributes||[]).map(a=>a.toLowerCase());
+		this[mixinClass.symbols.propsConfigByAttribute] = {};
+		Object.entries(propsConfig)
+		.sort(([a,ac],[b,bc])=>{
+			let ao = 'order' in ac, bo = 'order' in bc;
 			if(ao && bo) return ac.order<bc.order ? -1 : (ac.order>bc.order ? 1 : 0);
 			return ao && !bo ? -1 : (!ao && bo ? 1 : 0);
 		})
-		.forEach((name)=>{
-			let config = propsConfig[name];
-			if('attribute' in config && typeof config.attribute!='string') throw new Error(`Unable to setup property '${name}' on ${this.constructor.name}. Attribute is not a string.`);
-			let attribute = ('attribute' in config ? config.attribute+'' : name).toLowerCase();
-			let combinedName = name==attribute || name.toLowerCase()==attribute ? name : name+'/'+attribute;
-			config.propertyName = name;
-			propsConfigByAttribute[attribute] = config;
-			if(protectedProperties.indexOf(name)!==-1) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. '${name}' is a protected property.`);
-			if(protectedAttributes.indexOf(attribute)!==-1) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. '${attribute}' is a protected attribute.`);
-			if(config.overrideExisting!==true){
-				let propExists = false, protoPath = [];
-				for(let parentClass of protoTree){
-					if(parentClass.constructor) protoPath.push(parentClass.constructor.name);
-					if(parentClass.hasOwnProperty(name)){
-						let descriptor = Object.getOwnPropertyDescriptor(parentClass,name);
-						if(!descriptor || !descriptor.set || !descriptor.configurable || descriptor.get){
-							propExists = true;
-							break;
-						}
+		.forEach(([name,config])=>{
+			mixinClass.ce_mixinConstructorSetupProp.apply(this,[{ name, config, protoTree, propsConfig, argOptions }]);
+		});
+	}
+	
+	static ce_mixinConstructorSetupProp({ name, config, protoTree, propsConfig, argOptions }){
+		let { protectedProperties=[], protectedAttributes=[], propertyStore={}, onPropertySet, propertyDefaults={} } = argOptions;
+		let propsConfigByAttribute = this[mixinClass.symbols.propsConfigByAttribute];
+		if('attribute' in config && typeof config.attribute!='string') throw new Error(`Unable to setup property '${name}' on ${this.constructor.name}. Attribute is not a string.`);
+		let attribute = ('attribute' in config ? config.attribute+'' : name).toLowerCase();
+		let combinedName = name==attribute || name.toLowerCase()==attribute ? name : name+'/'+attribute;
+		config.propertyName = name;
+		propsConfigByAttribute[attribute] = config;
+		if(protectedProperties.indexOf(name)!==-1) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. '${name}' is a protected property.`);
+		if(protectedAttributes.indexOf(attribute)!==-1) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. '${attribute}' is a protected attribute.`);
+		if(config.overrideExisting!==true){
+			let propExists = false, protoPath = [];
+			for(let parentClass of protoTree){
+				if(parentClass.constructor) protoPath.push(parentClass.constructor.name);
+				if(parentClass.hasOwnProperty(name)){
+					let descriptor = Object.getOwnPropertyDescriptor(parentClass,name);
+					if(!descriptor || !descriptor.set || !descriptor.configurable || descriptor.get){
+						propExists = true;
+						break;
 					}
 				}
-				if(propExists) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. It already exists on ${protoPath.join('=>')}.`);
 			}
-			if(name!==attribute && name.toLowerCase()!==attribute){
-				Object.keys(propsConfig).forEach((name2)=>{
-					if(name==name2) return;
-					let config2 = propsConfig[name2];
-					let attribute2 = ('attribute' in config2 ? config2.attribute+'' : name2).toLowerCase();
-					let combinedName2 = name2==attribute2 || name2.toLowerCase()==attribute2 ? name2 : name2+'/'+attribute2;
-					if(attribute==attribute2) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. It already exists as a different property/attribute '${combinedName2}'.`);
-				});
-			}
-			if(hasConfigDefaults) for(let k in propertyDefaults){ if(!(k in config))config[k]=propertyDefaults[k]; }
-			let hasObserver = 'observer' in config && (typeof config.observer==='function' || typeof config.observer==='string');
-			let isObserverString = hasObserver && typeof config.observer==='string';
-			let eventName = typeof config.notify==='string' ? config.notify : (config.notify ? name+'-changed' : null);
-			if(typeof config.type==='string'){
-				if(config.type in propTypes) config.type = propTypes[config.type];
-				else throw new Error("'"+config.type+"' is not a valid propType");
-			}
-			else if(config.type===String) config.type = propTypes.StringLegacy;
-			else if(config.type===Number) config.type = propTypes.NumberLegacy;
-			else if(config.type===Boolean) config.type = propTypes.Boolean;
-			let reflectToAttribute = false, reflectFromAttribute = false, reflectFromProperty = false;
-			let transformToAttribute = null, transformFromAttribute = null, transformFromProperty = null;
-			if(config.type){
-				if(!config.type.toAttribute || !(typeof config.type.toAttribute==='function') || !config.type.fromAttribute || !(typeof config.type.fromAttribute==='function') || !config.type.fromProperty || !(typeof config.type.fromProperty==='function')) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. Type has missing or invalid toAttribute, fromAttribute or fromProperty transform functions.`);
-				transformToAttribute = config.type.toAttribute;
-				transformFromAttribute = config.type.fromAttribute;
-				transformFromProperty = config.type.fromProperty;
-				reflectToAttribute = reflectFromAttribute = reflectFromProperty = true;
-			}
-			if('reflectToAttribute' in config){
-				reflectToAttribute = !!config.reflectToAttribute;
-				if(typeof config.reflectToAttribute==='function') transformToAttribute = config.reflectToAttribute;
-				else if(!config.reflectToAttribute) transformToAttribute = null;
-			}
-			if(config.readOnly){ reflectFromAttribute = false; transformFromAttribute = null; }
-			else if('reflectFromAttribute' in config){
-				reflectFromAttribute = !!config.reflectFromAttribute;
-				if(typeof config.reflectFromAttribute==='function') transformFromAttribute = config.reflectFromAttribute;
-				else if(!config.reflectFromAttribute) transformFromAttribute = null;
-			}
-			if(config.readOnly){ reflectFromProperty = false; transformFromProperty = null; }
-			else if('reflectFromProperty' in config){
-				reflectFromProperty = !!config.reflectFromProperty;
-				if(typeof config.reflectFromProperty==='function') transformFromProperty = config.reflectFromProperty;
-				else if(!config.reflectFromProperty) transformFromProperty = null;
-			}
-			else if(!config.type && !reflectFromProperty) reflectFromProperty = true; // Backwards compatibility & default behavior
-			let setDescriptors = [];
-			for(let classObj of protoTree){
-				let descriptor = Object.getOwnPropertyDescriptor(classObj,name);
-				if(descriptor && descriptor.set && setDescriptors.indexOf(descriptor.set)===-1) setDescriptors.unshift(descriptor.set);
-			}
-			let eProp = config[mixinClass.symbols.elementProperty] = new elementProperty({
-				propertyStore, element, name, attribute, config, reflectToAttribute, reflectFromAttribute, reflectFromProperty, transformToAttribute, transformFromAttribute, transformFromProperty, onPropertySet, hasObserver, isObserverString, eventName, setDescriptors
+			if(propExists) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. It already exists on ${protoPath.join('=>')}.`);
+		}
+		if(name!==attribute && name.toLowerCase()!==attribute){
+			Object.entries(propsConfig).forEach(([name2,config2])=>{
+				if(name==name2) return;
+				let attribute2 = ('attribute' in config2 ? config2.attribute+'' : name2).toLowerCase();
+				let combinedName2 = name2==attribute2 || name2.toLowerCase()==attribute2 ? name2 : name2+'/'+attribute2;
+				if(attribute==attribute2) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. It already exists as a different property/attribute '${combinedName2}'.`);
 			});
-			if(transformFromProperty){
-				config.value = mixinClass.fn(transformFromProperty,element,['value' in config ? config.value : void 0]);
-			}
-			let defaultValue = config.value;
-			let existingDescriptor = Object.getOwnPropertyDescriptor(element,name);
-			let existingPropExists = existingDescriptor && existingDescriptor.enumerable;
-			let existingPropValue = existingPropExists ? (existingDescriptor.get ? existingDescriptor.get() : existingDescriptor.value) : void 0;
-			if(existingPropExists && transformFromProperty) existingPropValue = mixinClass.fn(transformFromProperty,element,[existingPropValue]);
-			if(config.readOnly) Object.defineProperty(element,name,{
-				enumerable: eProp.enumerable,
-				configurable: eProp.configurable,
-				writable: false,
-				value: eProp.get()
-			});
-			else Object.defineProperty(element,name,eProp);
-			let reflectToAttributeInConstructor = 'reflectToAttributeInConstructor' in config ? !!config.reflectToAttributeInConstructor : true;
-			let delayChangeInConstructor = 'delayChangeInConstructor' in config ? !!config.delayChangeInConstructor : true;
-			let attribExists = this.hasAttribute(attribute);
-			let attribValue = eProp.getValueFromAttribute();
-			if(attribExists) eProp.skipNextAttribChange[attribute] = attribValue;
-			if(existingPropExists && attribExists) existingPropExists = false;
-			if(!existingPropExists && !attribExists && reflectToAttribute && reflectToAttributeInConstructor && defaultValue!==null){
-				eProp.reflectValueToAttribute(defaultValue); // Set default value
-			}
-			if((existingPropExists && !attribExists && defaultValue!==existingPropValue) || (!existingPropExists && attribExists && defaultValue!==attribValue) || (!existingPropExists && !attribExists && defaultValue!==void 0)){
-				if(!(name in propertyStore)) propertyStore[name] = defaultValue; // Set default value
-			}
-			if(delayChangeInConstructor){
-				eProp.ignoreEmitChange = true;
-				Promise.resolve().then(()=>{
-					eProp.ignoreEmitChange = false;
-					let nowValue = eProp.get();
-					if(nowValue!==defaultValue && !eProp.firstChangeEmitted) eProp.emitChange(defaultValue,nowValue);
-				});
-			}
-			if(!reflectToAttributeInConstructor && reflectToAttribute) eProp.props.reflectToAttribute = false;
-			if(existingPropExists && !attribExists && reflectFromProperty && defaultValue!==existingPropValue){
-				eProp.set(existingPropValue); // Set new value
-			}
-			if(!existingPropExists && attribExists && reflectFromAttribute && defaultValue!==attribValue){
-				eProp.setValueViaAttribute(attribValue); // Set new value
-			}
-			if(!reflectToAttributeInConstructor && reflectToAttribute) eProp.props.reflectToAttribute = true;
-			Object.freeze(config);
+		}
+		for(let k in propertyDefaults){ if(!(k in config))config[k]=propertyDefaults[k]; }
+		let hasObserver = 'observer' in config && (typeof config.observer==='function' || typeof config.observer==='string');
+		let isObserverString = hasObserver && typeof config.observer==='string';
+		let eventName = typeof config.notify==='string' ? config.notify : (config.notify ? name+'-changed' : null);
+		if(typeof config.type==='string'){
+			if(config.type in propTypes) config.type = propTypes[config.type];
+			else throw new Error("'"+config.type+"' is not a valid propType");
+		}
+		else if(config.type===String) config.type = propTypes.StringLegacy;
+		else if(config.type===Number) config.type = propTypes.NumberLegacy;
+		else if(config.type===Boolean) config.type = propTypes.Boolean;
+		let reflectToAttribute = false, reflectFromAttribute = false, reflectFromProperty = false;
+		let transformToAttribute = null, transformFromAttribute = null, transformFromProperty = null;
+		if(config.type){
+			if(!config.type.toAttribute || !(typeof config.type.toAttribute==='function') || !config.type.fromAttribute || !(typeof config.type.fromAttribute==='function') || !config.type.fromProperty || !(typeof config.type.fromProperty==='function')) throw new Error(`Unable to setup property/attribute '${combinedName}' on ${this.constructor.name}. Type has missing or invalid toAttribute, fromAttribute or fromProperty transform functions.`);
+			transformToAttribute = config.type.toAttribute;
+			transformFromAttribute = config.type.fromAttribute;
+			transformFromProperty = config.type.fromProperty;
+			reflectToAttribute = reflectFromAttribute = reflectFromProperty = true;
+		}
+		if('reflectToAttribute' in config){
+			reflectToAttribute = !!config.reflectToAttribute;
+			if(typeof config.reflectToAttribute==='function') transformToAttribute = config.reflectToAttribute;
+			else if(!reflectToAttribute) transformToAttribute = null;
+		}
+		if(config.readOnly){ reflectFromAttribute = false; transformFromAttribute = null; }
+		else if('reflectFromAttribute' in config){
+			reflectFromAttribute = !!config.reflectFromAttribute;
+			if(typeof config.reflectFromAttribute==='function') transformFromAttribute = config.reflectFromAttribute;
+			else if(!reflectFromAttribute) transformFromAttribute = null;
+		}
+		if(config.readOnly){ reflectFromProperty = false; transformFromProperty = null; }
+		else if('reflectFromProperty' in config){
+			reflectFromProperty = !!config.reflectFromProperty;
+			if(typeof config.reflectFromProperty==='function') transformFromProperty = config.reflectFromProperty;
+			else if(!reflectFromProperty) transformFromProperty = null;
+		}
+		else if(!config.type && !reflectFromProperty) reflectFromProperty = true; // Backwards compatibility & default behavior
+		let setDescriptors = [];
+		for(let classObj of protoTree){
+			let descriptor = Object.getOwnPropertyDescriptor(classObj,name);
+			if(descriptor && descriptor.set && setDescriptors.indexOf(descriptor.set)===-1) setDescriptors.unshift(descriptor.set);
+		}
+		let eProp = config[mixinClass.symbols.elementProperty] = new elementProperty({
+			propertyStore, element:this, name, attribute, config, reflectToAttribute, reflectFromAttribute, reflectFromProperty, transformToAttribute, transformFromAttribute, transformFromProperty, onPropertySet, hasObserver, isObserverString, eventName, setDescriptors
 		});
+		if(transformFromProperty){
+			config.value = mixinClass.fn(transformFromProperty,this,['value' in config ? config.value : void 0]);
+		}
+		let defaultValue = config.value;
+		let existingDescriptor = Object.getOwnPropertyDescriptor(this,name);
+		let existingPropExists = existingDescriptor && existingDescriptor.enumerable;
+		let existingPropValue = existingPropExists ? (existingDescriptor.get ? existingDescriptor.get() : existingDescriptor.value) : void 0;
+		if(existingPropExists && transformFromProperty) existingPropValue = mixinClass.fn(transformFromProperty,this,[existingPropValue]);
+		if(config.readOnly) Object.defineProperty(this,name,{
+			enumerable: eProp.enumerable,
+			configurable: eProp.configurable,
+			writable: false,
+			value: eProp.get()
+		});
+		else Object.defineProperty(this,name,eProp);
+		let reflectToAttributeInConstructor = 'reflectToAttributeInConstructor' in config ? !!config.reflectToAttributeInConstructor : true;
+		let delayChangeInConstructor = 'delayChangeInConstructor' in config ? !!config.delayChangeInConstructor : true;
+		let attribExists = this.hasAttribute(attribute);
+		let attribValue = eProp.getValueFromAttribute();
+		if(attribExists) eProp.skipNextAttribChange[attribute] = attribValue;
+		if(existingPropExists && attribExists) existingPropExists = false;
+		if(!existingPropExists && !attribExists && reflectToAttribute && reflectToAttributeInConstructor && defaultValue!==null){
+			eProp.reflectValueToAttribute(defaultValue); // Set default value
+		}
+		if((existingPropExists && !attribExists && defaultValue!==existingPropValue) || (!existingPropExists && attribExists && defaultValue!==attribValue) || (!existingPropExists && !attribExists && defaultValue!==void 0)){
+			if(!(name in propertyStore)) propertyStore[name] = defaultValue; // Set default value
+		}
+		if(delayChangeInConstructor){
+			eProp.ignoreEmitChange = true;
+			Promise.resolve().then(()=>{
+				eProp.ignoreEmitChange = false;
+				let nowValue = eProp.get();
+				if(nowValue!==defaultValue && !eProp.firstChangeEmitted) eProp.emitChange(defaultValue,nowValue);
+			});
+		}
+		if(!reflectToAttributeInConstructor && reflectToAttribute) eProp.props.reflectToAttribute = false;
+		if(existingPropExists && !attribExists && reflectFromProperty && defaultValue!==existingPropValue){
+			eProp.set(existingPropValue); // Set new value
+		}
+		if(!existingPropExists && attribExists && reflectFromAttribute && defaultValue!==attribValue){
+			eProp.setValueViaAttribute(attribValue); // Set new value
+		}
+		if(!reflectToAttributeInConstructor && reflectToAttribute) eProp.props.reflectToAttribute = true;
+		Object.freeze(config);
 	}
 
 	static applyCEMixin(base,propertiesName='properties',mixinConfig={}){
