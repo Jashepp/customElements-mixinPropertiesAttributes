fb24a9cb1eea7adc9b54d27b89bd0c8f79b8dad5
Jason <unchosen.aus@gmail.com>
2024-10-31 03:49:41 +0800

    Remove test runs with 3rd-party dependencies

--- tests/99-examples.test.js
+++ tests/99-examples.test.js
@@ -28,34 +28,7 @@ describe("Examples",()=>{
 				expect(e.innerText,'.innerText').to.eq('Hello, Beautiful World!');
 			});
 	});
-
-	it("2-lit-html",()=>{
-		cy.visit('../examples/2-lit-html.html');
-		cy.get('example-hello')
-			.should(([e])=>{ // use 'should' to wait for queued render
-				expect(e.innerText,'.innerText').to.eq('');
-				expect(e.shadowRoot).to.exist;
-				let span = e.shadowRoot.querySelector('span');
-				expect(span.innerText,'.shadowRoot -> span.innerText').to.eq('Hello, Developer!');
-			})
-			.then(([e])=>{
-				e.name = "Foo Bar Prop"
-			});
-		cy.get('example-hello')
-			.should(([e])=>{ // use 'should' to wait for queued render
-				let span = e.shadowRoot.querySelector('span');
-				expect(span.innerText,'.shadowRoot -> span.innerText').to.eq('Hello, Foo Bar Prop!');
-			})
-			.then(([e])=>{
-				e.setAttribute('name',"Foo Bar Attrib");
-			});
-		cy.get('example-hello')
-			.should(([e])=>{ // use 'should' to wait for queued render
-				let span = e.shadowRoot.querySelector('span');
-				expect(span.innerText,'.shadowRoot -> span.innerText').to.eq('Hello, Foo Bar Attrib!');
-			})
-	});
-
+	
 	it("3-after-declared-arg",()=>{
 		cy.visit('../examples/3-after-declared-arg.html');
 		cy.get('example-toggle').parent()
